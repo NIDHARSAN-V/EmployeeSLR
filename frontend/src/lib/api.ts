74121f81bcde6b0b207a35649f6ed689922f8f94
Nidharsan Velmurugan <nidharsanvelmurugan@jmangroup.com>
2026-03-11 00:16:30 +0530

    loacl to pubip

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ?? "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ?? "http://135.235.195.238:8000";
 
 /**
  * Build a fully-qualified API URL.
