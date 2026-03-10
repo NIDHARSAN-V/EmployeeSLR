a89493b30f6b8e351b2660dac7f2644f49f2c217
Nidharsan Velmurugan <nidharsanvelmurugan@jmangroup.com>
2026-03-10 18:52:04 +0530

    proceeeding with seed file + key vault

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 // ✅ CORS CONFIG (IMPORTANT)
 app.use(
   cors({
-    origin: "http://localhost:3000", // frontend URL
+    origin: ["http://localhost:3000" , "http://localhost"], // frontend URL
     credentials: true, // allow cookies
   })
 );
