7548e90a74647011cbade0c72066aed227cd4e52
Nidharsan Velmurugan <nidharsanvelmurugan@jmangroup.com>
2026-03-11 00:45:55 +0530

    with the vm ip

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ?? "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ?? "http://98.70.28.134:8000";
 
 /**
  * Build a fully-qualified API URL.
