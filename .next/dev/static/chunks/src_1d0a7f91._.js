36a9addfede692eb35ad95428f471409b4b98d60
Nidharsan Velmurugan <nidharsanvelmurugan@jmangroup.com>
2026-03-11 10:31:19 +0530

    finalseeding

--- .next/dev/static/chunks/src_1d0a7f91._.js
+++ .next/dev/static/chunks/src_1d0a7f91._.js
@@ -9,7 +9,7 @@ __turbopack_context__.s([
     ()=>apiUrl
 ]);
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$polyfills$2f$process$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = /*#__PURE__*/ __turbopack_context__.i("[project]/node_modules/next/dist/build/polyfills/process.js [app-client] (ecmascript)");
-const API_BASE_URL = __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$polyfills$2f$process$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"].env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ?? "http://localhost:8000";
+const API_BASE_URL = ("TURBOPACK compile-time value", "/api")?.replace(/\/+$/, "") ?? "http://localhost:8000";
 function apiUrl(path) {
     const trimmedPath = path.replace(/^\//, "");
     return `${API_BASE_URL}/${trimmedPath}`;
